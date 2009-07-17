e35bdf51e82967c6e1dcd4c1241af189c8410226
tad <tad@jambus.local>
2009-07-17 19:18:46 +0000

    removed
    // mimicks li:hover for IE, which doesn't support it
    
    script as it causes js error in IE, no longer current believe to be legacy for AIGA build of CMS.

--- code/webroot/common/scripts/common.js
+++ code/webroot/common/scripts/common.js
@@ -83,20 +83,6 @@ function addBookmark(url,title){
 		return true; }	
 }
 
-// mimicks li:hover for IE, which doesn't support it
-sfHover = function() {
-	var sfEls = document.getElementById("tools").getElementsByTagName("LI");
-	for (var i=0; i<sfEls.length; i++) {
-		sfEls[i].onmouseover=function() {
-			this.className+=" sfhover";
-		}
-		sfEls[i].onmouseout=function() {
-			this.className=this.className.replace(new RegExp(" sfhover\\b"), "");
-		}
-	}
-}
-if (window.attachEvent) window.attachEvent("onload", sfHover);
-
 // recent popular module toggling
 function togglePopular() {
 	if(document.getElementById && document.createTextNode) {
